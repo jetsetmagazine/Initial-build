5821d5e41e02656391c0f982de90ae5e0052a078
Brandon <brandon@jetsetmag.com>
2013-11-09 11:47:43 -0700

    updated feature images
    updated feature images

--- js/global.js
+++ js/global.js
@@ -1,10 +1,20 @@
 jQuery(document).ready(function(){
 $(".articleImages li").hover(
 function(){
-$(this).find(".imageCaption").animate({opacity: "show", top:"0"}, "2500");
+$(this).find(".imageCaption").animate({opacity: "show", bottom:"0"}, "2500");
 },
 
 function(){
-$(this).find(".imageCaption").animate({opacity: "hide", top:"-100"}, "100");
+$(this).find(".imageCaption").animate({opacity: "hide", bottom:"-100"}, "100");
 });
+
+$(".articleImages li").hover(
+function(){
+$(this).find(".featureCaption").animate({opacity: "show", top:"0"}, "2500");
+},
+
+function(){
+$(this).find(".featureCaption").animate({opacity: "hide", top:"-100"}, "100");
+});
+
 });
