05d42e341a4ceb746bf41218439a1198e71ec8db
Brandon <brandon@jetsetmag.com>
2013-11-11 10:52:58 -0700

    Footer Buildout
    Added footer, created article template, began building styles out for
    article content

--- js/global.js
+++ js/global.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function(){
+
+
 $(".articleImages li").hover(
 function(){
 $(this).find(".imageCaption").animate({opacity: "show", bottom:"0"}, "2500");
@@ -8,6 +10,7 @@ function(){
 $(this).find(".imageCaption").animate({opacity: "hide", bottom:"-100"}, "100");
 });
 
+
 $(".articleImages li").hover(
 function(){
 $(this).find(".featureCaption").animate({opacity: "show", top:"0"}, "2500");
@@ -16,5 +19,25 @@ $(this).find(".featureCaption").animate({opacity: "show", top:"0"}, "2500");
 function(){
 $(this).find(".featureCaption").animate({opacity: "hide", top:"-100"}, "100");
 });
+
+
+
+$( "#category" ).click(function() {
+  $( ".catNav" ).fadeToggle( "fast");  
+  $( ".catNavFade" ).delay(250).fadeToggle(1000);
+  $(".catImg").hide(); 
+});
+
+
+/* Category Navigation Function */
+
+
+
+
+
+
+
+/*  END Category Navigation Function */
+
 
 });
